c589bf9e1de58c27985cbe07fbfd203301d05781
ManeeshWije <m.mwije1@gmail.com>
2023-05-28 14:49:42 -0400

    ??

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Terminal from './components/Terminal.tsx';
 import Distributive from './components/Distributive.tsx';
 import Ncr from './components/Ncr.tsx';
