f4f1279d28324fcd9ace97f67fdf36cc457d9e2c
ManeeshWije <m.mwije1@gmail.com>
2023-06-25 22:16:39 -0400

    smol

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -1,35 +1,35 @@
-import React, { useState, useRef, useEffect } from 'react';
-import me from '../../src/assets/me.jpg';
+import React, { useState, useRef, useEffect } from "react";
+import me from "../../src/assets/me.jpg";
 
 const Terminal: React.FC = () => {
     const goToDistributive = () => {
-        window.open('/#/distributive', '_blank');
+        window.open("/#/distributive", "_blank");
     };
 
     const goToNCR = () => {
-        window.open('/#/ncr', '_blank');
+        window.open("/#/ncr", "_blank");
     };
 
     const goToTulip = () => {
-        window.open('/#/tulip', '_blank');
+        window.open("/#/tulip", "_blank");
     };
 
-    const [input, setInput] = useState('');
+    const [input, setInput] = useState("");
     const [output, setOutput] = useState<string[]>([]);
     const [showPortrait, setShowPortrait] = useState(false);
-    const [currentDirectory, setCurrentDirectory] = useState('/Users/maneesh');
+    const [currentDirectory, setCurrentDirectory] = useState("/Users/maneesh");
 
     const terminalRef = useRef<HTMLDivElement>(null);
 
     const validDirectories = [
-        '/Users/maneesh/Pictures',
-        '/Users/maneesh/Documents',
-        '/Users/maneesh/Documents/work_experience',
-        '/Users/maneesh',
-        '/Pictures',
-        '/Documents',
-        '/Users',
-        '/',
+        "/Users/maneesh/Pictures",
+        "/Users/maneesh/Documents",
+        "/Users/maneesh/Documents/work_experience",
+        "/Users/maneesh",
+        "/Pictures",
+        "/Documents",
+        "/Users",
+        "/",
     ];
 
     useEffect(() => {
@@ -46,45 +46,45 @@ const Terminal: React.FC = () => {
     };
 
     const handleInputKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === 'Enter') {
+        if (e.key === "Enter") {
             e.preventDefault();
             setOutput((prevOutput) => [...prevOutput, `> ${input}`]);
             parseCommand(input);
-            setInput('');
+            setInput("");
         }
     };
 
     // this is disgusting
     const parseCommand = (input: string) => {
-        const args = input.split(' ');
+        const args = input.split(" ");
 
-        if (args[0] === 'ls') {
+        if (args[0] === "ls") {
             if (args.length === 1) {
                 let files: string[];
-                if (currentDirectory === '/Users/maneesh') {
-                    files = ['Pictures', 'Documents'];
-                } else if (currentDirectory === '/Users/maneesh/Pictures') {
-                    files = ['me.jpg'];
-                } else if (currentDirectory === '/Users/maneesh/Documents') {
+                if (currentDirectory === "/Users/maneesh") {
+                    files = ["Pictures", "Documents"];
+                } else if (currentDirectory === "/Users/maneesh/Pictures") {
+                    files = ["me.jpg"];
+                } else if (currentDirectory === "/Users/maneesh/Documents") {
                     files = [
-                        'about.txt',
-                        'contact.txt',
-                        'fun_fact.txt',
-                        'work_experience',
+                        "about.txt",
+                        "contact.txt",
+                        "fun_fact.txt",
+                        "work_experience",
                     ];
                 } else if (
                     currentDirectory ===
-                    '/Users/maneesh/Documents/work_experience'
+                    "/Users/maneesh/Documents/work_experience"
                 ) {
-                    files = ['distributive.html', 'ncr.html', 'tulip.html'];
-                } else if (currentDirectory === '/Users') {
-                    files = ['maneesh'];
-                } else if (currentDirectory === '/') {
-                    files = ['Users'];
+                    files = ["distributive.html", "ncr.html", "tulip.html"];
+                } else if (currentDirectory === "/Users") {
+                    files = ["maneesh"];
+                } else if (currentDirectory === "/") {
+                    files = ["Users"];
                 } else {
                     files = [];
                 }
-                setOutput((prevOutput) => [...prevOutput, files.join(' ')]);
+                setOutput((prevOutput) => [...prevOutput, files.join(" ")]);
             } else {
                 setOutput((prevOutput) => [
                     ...prevOutput,
@@ -93,16 +93,16 @@ const Terminal: React.FC = () => {
                     }`,
                 ]);
             }
-        } else if (args[0] === 'clear') {
+        } else if (args[0] === "clear") {
             if (args.length === 1) {
-                const h1Tags = document.getElementsByTagName('h1');
-                const imageTags = document.getElementsByTagName('img');
+                const h1Tags = document.getElementsByTagName("h1");
+                const imageTags = document.getElementsByTagName("img");
                 Array.from(h1Tags).forEach((h1Tag) => {
-                    h1Tag.innerHTML = '';
+                    h1Tag.innerHTML = "";
                 });
                 Array.from(imageTags).forEach((imageTag) => {
-                    imageTag.removeAttribute('src');
-                    imageTag.style.display = 'none';
+                    imageTag.removeAttribute("src");
+                    imageTag.style.display = "none";
                     setShowPortrait(false);
                 });
                 setOutput(() => []);
@@ -114,12 +114,12 @@ const Terminal: React.FC = () => {
                     }`,
                 ]);
             }
-        } else if (args[0] === 'help') {
+        } else if (args[0] === "help") {
             if (args.length === 1) {
                 const intro: string =
-                    'only a small subset of bash commands are implemented and some functionalities may not exist such as flag options. The list is as follows: ';
+                    "only a small subset of bash commands are implemented and some functionalities may not exist such as flag options. The list is as follows: ";
                 const set: string =
-                    'ls, cd, pwd, clear, cat (only works with text files), open (similar to xdg-open on Linux and open on MacOS)';
+                    'ls (list), cd (change directory), pwd (print working directory), clear (clear the screen), cat (output a text file), open (similar to "xdg-open" on Linux and "open" on MacOS. *Use it on images or html files)';
                 setOutput((prevOutput) => [...prevOutput, intro, set]);
             } else {
                 setOutput((prevOutput) => [
@@ -129,17 +129,17 @@ const Terminal: React.FC = () => {
                     }`,
                 ]);
             }
-        } else if (args[0] === 'cd') {
+        } else if (args[0] === "cd") {
             let newDirectory: string;
             if (args.length === 2) {
-                if (args[1] === '..') {
+                if (args[1] === "..") {
                     // move to the parent directory
-                    const parts = currentDirectory.split('/').filter(Boolean);
+                    const parts = currentDirectory.split("/").filter(Boolean);
                     parts.pop();
-                    newDirectory = '/' + parts.join('/');
+                    newDirectory = "/" + parts.join("/");
                 } else {
                     // move to a relative directory if it exists
-                    newDirectory = currentDirectory + '/' + args[1];
+                    newDirectory = currentDirectory + "/" + args[1];
                 }
                 // check if the new directory exists
                 if (validDirectories.includes(newDirectory)) {
@@ -155,14 +155,14 @@ const Terminal: React.FC = () => {
                     ]);
                 }
             } else {
-                newDirectory = '/Users/maneesh';
+                newDirectory = "/Users/maneesh";
                 setCurrentDirectory(newDirectory);
                 setOutput((prevOutput) => [
                     ...prevOutput,
                     `Changed directory to: ${newDirectory}`,
                 ]);
             }
-        } else if (args[0] === 'pwd') {
+        } else if (args[0] === "pwd") {
             if (args.length == 1) {
                 setOutput((prevOutput) => [
                     ...prevOutput,
@@ -176,19 +176,19 @@ const Terminal: React.FC = () => {
                     }`,
                 ]);
             }
-        } else if (args[0] === 'cat') {
+        } else if (args[0] === "cat") {
             if (args.length == 2) {
                 if (
-                    currentDirectory === '/' ||
-                    currentDirectory === '/Users' ||
-                    currentDirectory === '/Users/maneesh'
+                    currentDirectory === "/" ||
+                    currentDirectory === "/Users" ||
+                    currentDirectory === "/Users/maneesh"
                 ) {
                     setOutput((prevOutput) => [
                         ...prevOutput,
                         `ERROR: cannot use cat on a directory.`,
                     ]);
-                } else if (currentDirectory === '/Users/maneesh/Documents') {
-                    if (args[1] === 'about.txt') {
+                } else if (currentDirectory === "/Users/maneesh/Documents") {
+                    if (args[1] === "about.txt") {
                         setOutput((prevOutput) => [
                             ...prevOutput,
                             `hi, I'm Maneesh, a Computer Science student studying at the University of
@@ -198,20 +198,20 @@ const Terminal: React.FC = () => {
                             `,
                             `You can check out my other projects here: <a href="https://www.github.com/ManeeshWije">www.github.com/ManeeshWije</a>`,
                         ]);
-                    } else if (args[1] === 'contact.txt') {
+                    } else if (args[1] === "contact.txt") {
                         setOutput((prevOutput) => [
                             ...prevOutput,
                             `you can contact me via email: <a href="mailto:m.mwije1@gmail.com">m.mwije1@gmail.com</a>`,
                             `or connect with me on LinkedIn: <a href="https://www.linkedin.com/in/maneeshwije/">https://www.linkedin.com/in/maneeshwije/</a>`,
                             `also follow me on Twitter: <a href="https://twitter.com/maneesh_wije">https://twitter.com/maneesh_wije</a>`,
                         ]);
-                    } else if (args[1] === 'fun_fact.txt') {
+                    } else if (args[1] === "fun_fact.txt") {
                         setOutput((prevOutput) => [
                             ...prevOutput,
                             `apart from programming, I also love to stay active by going
                             climbing whether it's the crag in the area or a local gym.
                             As of now, I mainly focus on bouldering and I've been doing
-                            it for around a year with my PB being V6 (7a). Overall, I
+                            it for around a year with my PB being V7 (7a+). Overall, I
                             greatly enjoy the problem-solving aspect of figuring out a
                             boulder problem and being able to push myself physically is
                             an added bonus.`,
@@ -223,8 +223,8 @@ const Terminal: React.FC = () => {
                         ]);
                     }
                 } else if (
-                    currentDirectory === '/Users/maneesh/Pictures' &&
-                    args[1] === 'me.jpg'
+                    currentDirectory === "/Users/maneesh/Pictures" &&
+                    args[1] === "me.jpg"
                 ) {
                     setOutput((prevOutput) => [
                         ...prevOutput,
@@ -232,7 +232,7 @@ const Terminal: React.FC = () => {
                     ]);
                 } else if (
                     currentDirectory ===
-                    '/Users/maneesh/Documents/work_experience'
+                    "/Users/maneesh/Documents/work_experience"
                 ) {
                     setOutput((prevOutput) => [
                         ...prevOutput,
@@ -252,10 +252,10 @@ const Terminal: React.FC = () => {
                     }`,
                 ]);
             }
-        } else if (args[0] === 'open') {
+        } else if (args[0] === "open") {
             if (args.length === 2) {
-                if (currentDirectory === '/Users/maneesh/Pictures') {
-                    if (args[1] === 'me.jpg') {
+                if (currentDirectory === "/Users/maneesh/Pictures") {
+                    if (args[1] === "me.jpg") {
                         setShowPortrait(true);
                     } else {
                         setOutput((prevOutput) => [
@@ -263,28 +263,28 @@ const Terminal: React.FC = () => {
                             `ERROR: file ${args[1]} does not exist.`,
                         ]);
                     }
-                } else if (currentDirectory === '/Users/maneesh/Documents') {
+                } else if (currentDirectory === "/Users/maneesh/Documents") {
                     setOutput((prevOutput) => [
                         ...prevOutput,
                         `ERROR: cannot use open on text files or directories`,
                     ]);
                 } else if (
                     currentDirectory ===
-                    '/Users/maneesh/Documents/work_experience'
+                    "/Users/maneesh/Documents/work_experience"
                 ) {
-                    if (args[1] === 'distributive.html') {
+                    if (args[1] === "distributive.html") {
                         setOutput((prevOutput) => [
                             ...prevOutput,
                             `redirecting...`,
                         ]);
                         goToDistributive();
-                    } else if (args[1] === 'ncr.html') {
+                    } else if (args[1] === "ncr.html") {
                         setOutput((prevOutput) => [
                             ...prevOutput,
                             `redirecting...`,
                         ]);
                         goToNCR();
-                    } else if (args[1] === 'tulip.html') {
+                    } else if (args[1] === "tulip.html") {
                         setOutput((prevOutput) => [
                             ...prevOutput,
                             `redirecting...`,
@@ -321,18 +321,18 @@ const Terminal: React.FC = () => {
     return (
         <>
             <div>
-                <h1 className='font-bold text-lg'>
+                <h1 className="font-bold text-lg">
                     welcome to my personal website!
                 </h1>
-                <h1 className='font-bold text-lg'>
+                <h1 className="font-bold text-lg">
                     type 'help' to see the implemented commands
                 </h1>
-                <h1 className='font-bold text-lg'>
-                    check out the source code for this project on my{' '}
-                    <a href='https://www.github.com/ManeeshWije'>github</a>
+                <h1 className="font-bold text-lg">
+                    check out the source code for this project on my{" "}
+                    <a href="https://www.github.com/ManeeshWije">github</a>
                 </h1>
             </div>
-            <div ref={terminalRef} className='output-container'>
+            <div ref={terminalRef} className="output-container">
                 <div>
                     {output.map((command, index) => (
                         <div
@@ -342,34 +342,34 @@ const Terminal: React.FC = () => {
                     ))}
                 </div>
             </div>
-            <div className='flex flex-row w-full'>
+            <div className="flex flex-row w-full">
                 <span>&gt;&#160;</span>
                 <span
-                    className='absolute'
+                    className="absolute"
                     style={{
                         transform: `translateX(${input.length * 9.6 + 20}px)`,
-                        animation: 'blink 1s infinite',
+                        animation: "blink 1s infinite",
                     }}
                 >
                     &#9611;
                 </span>
                 <input
-                    type='text'
+                    type="text"
                     value={input}
                     onChange={handleInputChange}
                     onKeyPress={handleInputKeyPress}
                     autoFocus={true}
-                    className='w-full bg-transparent caret-transparent'
+                    className="w-full bg-transparent caret-transparent"
                 />
             </div>
-            <div className='absolute top-20 right-0 p-4' id='portrait-img'>
+            <div className="absolute top-20 right-0 p-4" id="portrait-img">
                 {showPortrait && (
                     <img
                         src={me}
                         width={450}
                         height={400}
-                        alt='self-portrait'
-                        className='border-pink-400 border rounded-lg'
+                        alt="self-portrait"
+                        className="border-pink-400 border rounded-lg"
                     />
                 )}
             </div>
